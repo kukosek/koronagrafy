32712da6291f66df9091eeb6e010ace0a677dae0
kukosek <kukys410@gmail.com>
2020-03-19 11:21:42 +0100

    Add license to about

---
+++ about_script.js
@@ -0,0 +1,12 @@
+var licenseReq = new XMLHttpRequest();
+licenseReq.onload = function(e) {
+    document.getElementById("licenseArea").innerHTML = licenseReq.responseText.replace("\n", "<br>\n"); // not responseText
+    /* ... */
+}
+licenseReq.onerror = function(e) {
+    document.getElementById("licenseArea").innerHTML = "ERROR: Could't load license"; // not responseText
+    /* ... */
+}
+
+licenseReq.open("GET", "/LICENSE");
+licenseReq.send();
