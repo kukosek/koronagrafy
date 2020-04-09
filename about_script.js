76ec824f7a0d0797b2d3ef96229d2a2f85e8cae5
kukosek <kukys410@gmail.com>
2020-04-09 11:42:56 +0200

    Hopefully fixes #12 the about license loading bug

--- about_script.js
+++ about_script.js
@@ -1,12 +1,24 @@
+function loadLicense(Req){
+    document.getElementById("licenseArea").innerHTML = Req.responseText.replace(/\n/g, "<br>\n");
+}
 var licenseReq = new XMLHttpRequest();
+var waiting = false;
 licenseReq.onload = function(e) {
-    document.getElementById("licenseArea").innerHTML = licenseReq.responseText.replace(/\n/g, "<br>\n"); // not responseText
-    /* ... */
+    if (document.readyState === "complete" || document.readyState === "interactive"){
+        loadLicense(licenseReq);
+    }else{
+        waiting = true;
+    }
 }
 licenseReq.onerror = function(e) {
-    document.getElementById("licenseArea").innerHTML = "ERROR: Could't load license"; // not responseText
-    /* ... */
+    document.getElementById("licenseArea").innerHTML = "ERROR: Could't load license";
 }
-
 licenseReq.open("GET", "/LICENSE");
 licenseReq.send();
+window.onload = (event) => {
+    if (waiting){
+        loadLicense(licenseReq);
+        waiting = false;
+    }
+};
+  
